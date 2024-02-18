d0724be5d6c04bfbf49574c23099c4b02e122dc8
piatas5 <piatas55@gmail.com>
2024-02-18 10:35:10 +0100

    Added more echos, added checking the number of directories

--- script-to-delete-tomcat-files.sh
+++ script-to-delete-tomcat-files.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
-
 echo "Welcome in the app! Here you can clear all tomcat files at once!"
 echo
 
-for directory in "/tmp/tomcat*";
-do
-rm -f $directory
-done
+DIRECTORY=/home/luke/Tomcat_Directories
+
+DIRCOUNT=`find $DIRECTORY -maxdepth 1 -mindepth 1 -type d -name "tomcat*" | wc -l`
+
+echo "Number of tomcat directories: $DIRCOUNT"
+echo "Do you want to delete them all? [y/n]"
+
+read answer
+
+if [ $answer == "y" ] || [ $answer == "Y" ]
+then
+	for dir in "/home/luke/Tomcat_Directories/tomcat*";
+	do
+	rm -r -f $dir
+	done
+if [ $DIRCOUNT == 0 ]
+then
+echo "No directories to delete!"
+else
+echo "Directories deleted successfully!"
+fi
+
+elif [ $answer == "n" ] || [ $answer == "N" ]
+then
+echo "Bye!"
+exit 0
+fi
